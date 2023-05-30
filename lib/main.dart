9dbcad73febc737ded0973f7d3909c3b26fb5314
Samuel Nittala <nittalasamuel7@gmail.com>
2023-05-31 00:48:04 +0530

    integrate welcome screen widget to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:splitit_app/src/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(const ProviderScope(child: MainApp()));
 }
